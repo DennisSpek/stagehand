2aa4329e937a9b430fa2ba53ea01e381470ab78f
Dennis Spek <dennisvdspek1@gmail.com>
2024-11-04 15:18:06 -0500

    Fixed build

--- src/app/@dashboard/[id]/layout.tsx
+++ src/app/@dashboard/[id]/layout.tsx
@@ -17,12 +17,6 @@ export default function ArtistLayout({
   return (
     <div className='h-full flex flex-col'>
       <main className='flex flex-1 gap-8'>
-        <div className='w-[288px] flex flex-col gap-4 h-full'>
-          <ArtistSelector />
-          <div className='flex-1 overflow-auto'>
-            <SideNavigation />
-          </div>
-        </div>
         <div className='flex flex-1 flex-col gap-4'>
           <WelcomeMessage title='Welcome back, UserName. 👋 Take a look at your stats!' sub='Last refresh: 00:00 mm-dd-yyyy CET (23h ago)' />
           {children}
